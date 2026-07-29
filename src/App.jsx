fbd60a8cae974249721c9670c40c3976261138b6
Bogdan3690 <dblevko@gmail.com>
2026-07-29 13:00:50 -0600

    add statistics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
 import Profile from './components/Profile'
+import Statistics from './components/Statistics'
 import user from "./data/profile.json"
+import stats from "./data/statistics.json"
 function App() {
 
   return (
     <>
     <Profile user={user} />
+    <Statistics stats={stats} />
     </>
   )
 }
---
+++ src/components/Statistics.jsx
@@ -0,0 +1,21 @@
+const Statistics = ( {stats} ) => {
+    return(
+        <section class="statistics">
+  <h2 class="title">Upload stats</h2>
+
+
+
+  <ul class="stat-list">
+    {stats.map((stat) => {
+        return(
+            <li key={stat.id} class="item">
+      <span class="label">{stat.label}</span>
+      <span class="percentage">{stat.percentage}</span>
+      </li>)
+    })}
+  </ul>
+</section>
+    )
+}
+
+export default Statistics
